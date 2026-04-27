a07aeac1821e2b0cc6699fd4aa5639832804b4ab
Amitrajit007 <amitrajitsarkar4@gmail.com>
2026-04-27 21:29:20 +0530

    feat: enhance root locus analysis with additional unity feedback systems

--- Control system/rootLocus.m
+++ Control system/rootLocus.m
@@ -1,7 +1,29 @@
-k=10;
-s = tf([10],[1 3 2 0]);
-[zero,pole,gain] = tf2zp([10],[1 3 2 0])
+k =10;
+[zero,pole,gain] = tf2zp([k] , [1 3 2 0])
+s = tf([k] ,[1 3 2 0]);
 rlocus(s)
 sgrid(0.7 ,1.8)
-[gain ,pole] = rlocfind(s)// then select a pt as close
-// to the imaginary axis and keeping all poles -ve
+rlocfind(s)
+
+// 2 unity feedback
+
+k1= 1;
+k2= 2;
+k3= 3;
+
+sys1 = tf(conv([k1] ,[1 1 2]) , [1 2 1]);
+sys2 = tf(conv([k2] ,[1 1 2]) , [1 2 1]);
+sys3 = tf(conv([k3] ,[1 1 2]) , [1 2 1]);
+
+rlocus(sys1);
+rlocus(sys2);
+rlocus(sys3); 
+
+// 3 open loop tf 
+
+n = conv(conv(conv([100],[1 1]),[1 2]),[1 3.5]) ;
+den =  conv(conv([1 0],[1 3]),[1 2 5]); 
+s= tf(n ,den);
+
+pzmap(s)
+
